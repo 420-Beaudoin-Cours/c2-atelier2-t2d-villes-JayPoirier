58337672d85a6d4ec8963e1cf095df5d5be41c48
JayPoirier <jermypoirier@hotmail.com>
2020-02-25 17:16:49 -0500

    Continuer

--- Villes.java
+++ Villes.java
@@ -1,3 +1,34 @@
 public class Villes {
+
+    public static void afficherTab(int[][] tab){
+            for(int i = 0; i < tab[0].length; i++ )
+                for ()
+                System.out.println(( tab[0][i] ));
+    }
+
+    public static int sommeUneLigne(int[][] tab){
+
+        return 0;
+    }
+
+    public static int moyenneUneLigne(int[][] tab){
+
+        return 0;
+    }
+
+    public static int moyenneUneCol(int[][] tab){
+
+        return 0;
+    }
+
+    public static int maxLigne(int[][] tab){
+
+        return 0;
+    }
+
+    public static int minLigne(int[][] tab){
+
+        return 0;
+    }
 }
 
